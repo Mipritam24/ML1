39f9faca7ecf7a64847c0dfe3328d9adbec750e4
Pritam Mishra <mipritam24@gmail.com>
2025-03-31 17:55:44 +0530

    Update Multiple Linear Regression.py

--- 2. MLR/Multiple Linear Regression.py
+++ 2. MLR/Multiple Linear Regression.py
@@ -1,13 +1,1 @@
-Multiple Linear Regression
 
-From warnings import filterwarnings
-filterwarnings("ignore")
-
-PROFIT ~  RND, ADMIN, MKT
-PROFIT = B0 +B1*RND + B2*ADMIN + B3*MKT
-
-Step 1 - Read the dataset
-
-import pandas as pd
-df = pd.read_csv("50_Startups.csv")
-df.head()
